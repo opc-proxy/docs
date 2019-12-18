76d21baf24d04176a1651d5d0d3c39c456930506
git hai rotto il cazzo <motherfucker@motherfucker.com>
2019-12-18 16:44:05 +0100

    uhmm

--- source/conf.py
+++ source/conf.py
@@ -24,6 +24,7 @@ author = 'Ale'
 # The full version, including alpha/beta/rc tags
 release = '0.0'
 
+master_doc = 'index'
 
 # -- General configuration ---------------------------------------------------
 
