96d7800ab7913b110a1f4f6251c9e27b2442f005
git hai rotto il cazzo <motherfucker@motherfucker.com>
2019-12-18 16:55:01 +0100

    maybe

--- source/conf.py
+++ source/conf.py
@@ -24,7 +24,6 @@ author = 'Ale'
 # The full version, including alpha/beta/rc tags
 release = '0.0'
 
-master_doc = 'index'
 
 # -- General configuration ---------------------------------------------------
 
@@ -42,13 +41,19 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
+master_doc = 'index'
 
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+import os
+on_rtd = os.environ.get('READTHEDOCS') == 'True'
+if on_rtd:
+    html_theme = 'default'
+else:
+    html_theme = 'nature'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
